06fc095c49b057b7050231da1bdc7e820d127154
Rizwan Asif <rizasif@gmail.com>
2018-07-31 13:39:26 +0500

    bservation space

--- acktr_cont.py
+++ acktr_cont.py
@@ -112,8 +112,7 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, num_timesteps,
     mean_logger = setup_logger("Mean Logger", "log/episode_mean.txt")
 
     # print("Filter shape:  ", env.observation_space.shape)
-    space = env.observation_space.shape
-    space[0] *= 2
+    space = (env.observation_space.shape[0]*2,)
     obfilter = ZFilter(space)
 
     max_pathlength = env.spec.timestep_limit
